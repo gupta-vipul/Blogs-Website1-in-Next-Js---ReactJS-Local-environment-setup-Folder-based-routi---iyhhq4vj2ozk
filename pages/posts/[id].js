a88932e2d65fe89b5568e3a7c4c9e62d7892fb80
Vipul Gupta <mail.vipulg@gmail.com>
2023-12-07 00:21:38 +0530

    Added the requirement code to solve

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,13 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 function PostDetail() {
-  // Complete the function
+  const {query} = useRouter();
+  console.log("data", query);
+  return (
+    <>
+      <h1>Post Detail: {query.id}</h1>
+    </>
+  )
 }
 
 export default PostDetail;
